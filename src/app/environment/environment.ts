ae573b4dbf1351e3ecdb5d3881b92f44572e52b5
Joscha <j.sattler@28apps.de>
2024-02-05 15:18:30 +0100

    CSS reset + environment implemented

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,8 @@
+/**
+ * Environment variables:
+ * - baseUrl: The base url for the backend api
+ */
+export const environment = {
+  baseUrl: 'https://nest.template.dev2.28apps-software.de/',
+  mapsApiKey: 'AIzaSyCokvPfNoLOKy5aPfPxrIQF_FpJ7U6YbLU',
+};
