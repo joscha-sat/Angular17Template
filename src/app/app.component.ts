13194cf045c49a9150d7604be7401b0e7aab4823
Joscha <j.sattler@28apps.de>
2024-02-05 15:36:11 +0100

    prettier, scss stuff, eslint added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TUI_SANITIZER, TuiRootModule } from "@taiga-ui/core";
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, TuiRootModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
-  title = 'Angular-Template-V2';
+  title = "Angular-Template-V2";
 }
 
