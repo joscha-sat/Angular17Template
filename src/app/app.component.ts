2e158745250186b52ed52ab0452db0c5da3b1dd4
Joscha <j.sattler@28apps.de>
2024-02-05 14:57:11 +0100

    Taiga UI implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TUI_SANITIZER, TuiRootModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
 })
 export class AppComponent {
   title = 'Angular-Template-V2';
 }
+
