dfbac77f3298c5b156a128e526fa8467b28dd24f
Joscha <j.sattler@28apps.de>
2024-02-06 12:29:37 +0100

    updated snackbar / notification service implemented

--- src/app/views/login.view/login.view.component.ts
+++ src/app/views/login.view/login.view.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject } from "@angular/core";
+import { Component } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
 import { TuiIslandModule } from "@taiga-ui/kit";
 import { LoginHeaderComponent } from "../../components/login/login-header/login-header.component";
 import { LoginFormComponent } from "../../components/login/login-form/login-form.component";
 import { TuiButtonModule } from "@taiga-ui/core";
-import { TuiSnackbarService } from "../../services/tui-snackbar.service";
 
 @Component({
   selector: "app-login.view",
@@ -14,10 +13,4 @@ import { TuiSnackbarService } from "../../services/tui-snackbar.service";
   styleUrl: "./login.view.component.scss",
 })
 export class LoginViewComponent {
-
-  snackbarService = inject(TuiSnackbarService);
-
-  openSnackbar() {
-    this.snackbarService.openSnackbar("success", "title", "content");
-  }
 }
