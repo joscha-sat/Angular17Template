74fbd24609302bcc0c7c4c28a0b7cfdeedda63ef
Joscha <j.sattler@28apps.de>
2024-02-06 14:59:04 +0100

    nav buttons displayed as active / inactive

--- src/app/components/navigation/nav-button/nav-button.component.ts
+++ src/app/components/navigation/nav-button/nav-button.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { NavItem } from "../../../enums/nav-items";
 import { TuiButtonModule, TuiHintModule, TuiSvgModule } from "@taiga-ui/core";
-import { Router, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: "app-nav-button",
@@ -11,23 +11,18 @@ import { Router, RouterLinkActive } from "@angular/router";
     TuiHintModule,
     TuiSvgModule,
     RouterLinkActive,
+    RouterLink,
   ],
   templateUrl: "./nav-button.component.html",
   styleUrl: "./nav-button.component.scss",
 })
 export class NavButtonComponent {
   @Input() navItem: NavItem | undefined;
-  @Input() selected: boolean = false; // Checks if the button is selected.
-
-  @Output() isClicked: EventEmitter<NavItem> = new EventEmitter<NavItem>(); // Emits the clicked button to the parent component.
 
   constructor(private router: Router) {
   }
 
   onClick() {
     this.router.navigate([this.navItem!.link]).then();
-    this.isClicked.emit(this.navItem);
   }
-
-
 }
