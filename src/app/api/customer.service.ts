3345025119f406f55b225dff47dfdfa01c928a7f
Joscha <j.sattler@28apps.de>
2024-03-19 06:44:21 +0100

    Refactor customer data fetching and manipulation
    Removed direct data passing to the customer table component and moved the data handling logic to within the component itself. Now, the customer data fetching is controlled through a fetch function, providing more encapsulation of the component. Additionally, the customer service methods now trigger a refresh of the data, ensuring the data is always up to date when changes occur.

--- src/app/api/customer.service.ts
+++ src/app/api/customer.service.ts
@@ -1,42 +1,49 @@
 import { Injectable } from '@angular/core';
-import { Customer } from '../other/models/Customer';
-import { GenericHttpService, ResponseWithRecords } from "./generic-http.service";
-import { Observable } from "rxjs";
+import { GenericHttpService, idTypes, ResponseWithRecords } from "./generic-http.service";
+import { Observable, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Customer } from "../other/models/Customer";
+
+
+type queryParams = {
+  skip?: number;
+  limit?: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService extends GenericHttpService {
-  element = "ein Kunde"
-  private readonly endpoint = 'customer';
+  endpoint = 'customer';
+  element = "Ein Kunde"
+  private _refreshCustomers = new Subject<void>();
+  public refreshCustomers$ = this._refreshCustomers.asObservable();
 
-  // ADD ONE Customer
-  addCustomer(addCustomerObject: Customer): Observable<Customer | Customer[] | null> {
-    return this.create<Customer>(this.endpoint, addCustomerObject, this.element);
+  getAllCustomers(queryParams?: queryParams): Observable<ResponseWithRecords<Customer>> {
+    return this.getAll<Customer>(this.endpoint, queryParams);
   }
 
-  // GET ALL Customers
-  getAllCustomers(): Observable<ResponseWithRecords<Customer>> {
-    return this.getAll<Customer>(this.endpoint);
+  createCustomer(customer: Customer | Customer[]): Observable<Customer | Customer[] | null> {
+    return this.create<Customer>(this.endpoint, customer, this.element).pipe(
+      tap(() => this._refreshCustomers.next())
+    );
   }
 
-  // GET ONE Customer BY ID
-  getCustomerById(id: string | number): Observable<Customer> {
-    return this.getOne<Customer>(this.endpoint, id);
+  updateCustomer(customer: Customer | Customer[], id: idTypes): Observable<Customer | Customer[] | null> {
+    return this.update<Customer>(this.endpoint, customer, id, this.element).pipe(
+      tap(() => this._refreshCustomers.next())
+    );
   }
 
-  // UPDATE ONE Customer BY ID
-  updateCustomerById(id: string | number, updateCustomerObject: Customer): Observable<Customer | Customer[] | null> {
-    return this.update<Customer>(this.endpoint, updateCustomerObject, id, this.element);
+  deleteOneCustomer(id: idTypes): Observable<unknown> {
+    return this.deleteOne(this.endpoint, id, this.element).pipe(
+      tap(() => this._refreshCustomers.next())
+    );
   }
 
-  // DELETE ONE Customer BY ID
-  deleteCustomerById(id: number | string): Observable<unknown> {
-    return this.deleteOne(this.endpoint, id, this.element);
-  }
-
-  // DELETE ALL Customers
-  deleteAllCustomers(): Observable<unknown> {
-    return this.deleteAll<unknown>(this.endpoint);
+  deleteAllCustomers(): Observable<Customer | Customer[] | null> {
+    return this.deleteAll<Customer>(this.endpoint).pipe(
+      tap(() => this._refreshCustomers.next())
+    );
   }
 }
--- src/app/components/customers/customer-table/customer-table.component.ts
+++ src/app/components/customers/customer-table/customer-table.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input, signal } from '@angular/core';
-import { BaseTableAsyncComponent } from "../../../shared/base-table-async/base-table-async.component";
-import { Observable } from "rxjs";
+import { Component, inject, signal } from '@angular/core';
+import {
+  BaseTableAsyncComponent,
+  FetchDataFunction
+} from "../../../shared/base-table-async/base-table-async.component";
+import { BehaviorSubject, switchMap } from "rxjs";
+import { CustomerService } from "../../../api/customer.service";
 import { Customer } from "../../../other/models/Customer";
 
 @Component({
@@ -13,8 +17,23 @@ import { Customer } from "../../../other/models/Customer";
   styleUrl: './customer-table.component.scss'
 })
 export class CustomerTableComponent {
-  @Input({ required: true }) customers$: Observable<Customer[]> | undefined;
+  customerService = inject(CustomerService)
 
   headers = signal<string[]>(["Vorname"]);
   columns = signal<string[]>(["firstName"]);
+  refresh$ = new BehaviorSubject(null);
+
+  fetchCustomersFn: FetchDataFunction<Customer> = (page: number, size: number) => {
+    return this.refresh$.pipe(
+      switchMap(() =>
+        this.customerService.getAllCustomers({ limit: size, skip: page * size })
+      )
+    );
+  };
+
+  refreshDataSubscription() {
+    this.customerService.refreshCustomers$.subscribe(() => {
+      this.refresh$.next(null);
+    });
+  }
 }
