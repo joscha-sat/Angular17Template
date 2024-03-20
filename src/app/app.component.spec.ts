dfedec0027f3b6915fefbe8a978ff954ba60c5c6
Joscha <j.sattler@28apps.de>
2024-03-20 22:51:40 +0100

    A new 'app.component.spec.ts' file was also added to provide test coverage for the app.

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        NoopAnimationsModule,
+        TranslateModule.forRoot(),
+        AppComponent,
+        NavigationComponent,
+        LoadingSpinnerComponent,
+        HttpClientTestingModule
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+});
