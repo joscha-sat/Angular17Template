d3bf78e747bcb6de51fc8c478a2b5a05224aa5f8
Joscha <j.sattler@28apps.de>
2024-05-25 03:37:50 +0200

    Update pre-commit hook commands
    Removed the ng test and ng lint commands in the .husky/pre-commit hook, replacing them with npm run lint. Also, the prettier command remains unchanged. This should simplify and streamline our pre-commit process.

---
+++ src/app/shared/case-search/case-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './case-search.component.html',
+  styleUrl: './case-search.component.scss',
+})
+export class CaseSearchComponent {}
