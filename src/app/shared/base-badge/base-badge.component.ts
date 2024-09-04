4360a11a3b0681fc531769c4bfeb7d3632e190ae
joscha-satt <j.sattler@28apps.de>
2024-09-04 15:05:55 +0200

    TAIGA UI migration from v3 > v4 , add tuiStatus as default in badge

--- src/app/shared/base-badge/base-badge.component.ts
+++ src/app/shared/base-badge/base-badge.component.ts
@@ -1,11 +1,11 @@
 import { Component, input } from '@angular/core';
-import { TuiBadge } from '@taiga-ui/kit';
+import { TuiBadge, TuiStatus } from '@taiga-ui/kit';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-base-badge',
   standalone: true,
-  imports: [TuiBadge, TranslateModule],
+  imports: [TuiBadge, TranslateModule, TuiStatus],
   templateUrl: './base-badge.component.html',
   styleUrl: './base-badge.component.scss',
 })
