37135d99577d2456d254a19aad0d5e2059f3c15f
joscha-satt <j.sattler@28apps.de>
2024-12-04 19:36:52 +0100

    Add urlContainsName method to UtilityService
    Introduce urlContainsName to check if the router URL contains a specified name. This utilizes RxJS operators to filter for NavigationEnd events and map the URL check result. Additionally, the Router is injected for use within the service.

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -1,10 +1,14 @@
-import { Injectable, WritableSignal } from '@angular/core';
+import { inject, Injectable, WritableSignal } from '@angular/core';
 import { DropDownItem } from '../other/types/DropDownItem.type';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UtilityService {
+  router = inject(Router);
+
   createDropdownItems(
     dataArray: WritableSignal<any[]>,
     idKey: string = 'id',
@@ -16,5 +20,12 @@ export class UtilityService {
         label: item[labelKey],
       };
     });
+  }
+
+  urlContainsName(name: string): Observable<boolean> {
+    return this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.router.url.includes(name)),
+    );
   }
 }
