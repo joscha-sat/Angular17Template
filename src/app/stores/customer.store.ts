1ae61e9de42b3a9edf884e31a47c825c79b78922
joscha-satt <j.sattler@28apps.de>
2025-01-05 04:21:38 +0100

    Add CustomersStore with state management and @ngrx/signals
    Introduced `CustomersStore` to manage customer data using state management with @ngrx/signals. Added CRUD methods for customer operations and updated dependencies in `package.json` and `package-lock.json` to include @ngrx/signals. This enhances the application's scalability and maintainability.

---
+++ src/app/stores/customer.store.ts
@@ -0,0 +1,104 @@
+import {
+  patchState,
+  signalState,
+  signalStore,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { inject } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+import { CustomerService } from '../api/customer.service';
+import { Customer } from '../other/models/Customer';
+import { ResponseWithRecords } from '../api/base-http-service/base-http.service';
+
+type CustomersState = {
+  customers: Customer[];
+  customer?: Customer;
+  filter: { queryParams: any; order: 'asc' | 'desc' };
+};
+
+const initialState = signalState<CustomersState>({
+  customers: [],
+  customer: undefined,
+  filter: { queryParams: {}, order: 'asc' },
+});
+
+async function apiRequestAndPatchStoreData<T>(
+  serviceCall: Observable<T>,
+  patchCallback: (data: T) => void,
+): Promise<T> {
+  const response = await lastValueFrom(serviceCall);
+  patchCallback(response);
+  return response;
+}
+
+export const CustomersStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withMethods((store, customerService = inject(CustomerService)) => ({
+    // GET ALL Customer
+    async getAllCustomersPromise(
+      queryParams?: any,
+    ): Promise<ResponseWithRecords<Customer>> {
+      return apiRequestAndPatchStoreData(
+        customerService.getAllCustomers(queryParams),
+        (getAllResponse) =>
+          patchState(store, { customers: getAllResponse.records }),
+      );
+    },
+
+    // GET ONE Customer
+    async getCustomerByIdPromise(id: string | number): Promise<Customer> {
+      return apiRequestAndPatchStoreData(
+        customerService.getCustomerById(id),
+        (customer) => patchState(store, { customer }),
+      );
+    },
+
+    // CREATE ONE Customer
+    async createOneCustomerPromise(customer: Customer): Promise<Customer> {
+      return apiRequestAndPatchStoreData(
+        customerService.createOneCustomer(customer),
+        (newCustomer) =>
+          patchState(store, {
+            customers: [...store.customers(), newCustomer],
+          }),
+      );
+    },
+
+    // UPDATE ONE Customer
+    async updateCustomerByIdPromise(
+      id: string | number,
+      customer: Customer,
+    ): Promise<Customer> {
+      return apiRequestAndPatchStoreData(
+        customerService.updateCustomerById(id, customer),
+        (updatedCustomer) =>
+          patchState(store, {
+            customers: store
+              .customers()
+              .map((item) => (item.id === id ? updatedCustomer : item)),
+          }),
+      );
+    },
+
+    // DELETE ONE Customer
+    async deleteCustomerByIdPromise(id: string | number) {
+      return apiRequestAndPatchStoreData(
+        customerService.deleteCustomerById(id),
+        () =>
+          patchState(store, {
+            customers: store.customers().filter((item) => item.id !== id),
+          }),
+      );
+    },
+
+    // DELETE ALL Customer
+    async deleteAllCustomersPromise() {
+      return apiRequestAndPatchStoreData(
+        customerService.deleteAllCustomers(),
+        () => patchState(store, { customers: [] }),
+      );
+    },
+  })),
+);
