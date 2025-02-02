b1300e887fa8b74e61e7ae7a7970c1406db34e10
joscha-satt <j.sattler@28apps.de>
2025-02-02 04:19:42 +0100

    Refactor customer table into its own component
    Extracted customer table logic from test-view to a new reusable CustomerTableComponent. Updated related views and stores to integrate the new component and improve code modularity. Enhanced template table component with RxJS-based cleanup for subscription handling.

--- src/app/components/customers/customer-table/customer-table.component.ts
+++ src/app/components/customers/customer-table/customer-table.component.ts
@@ -6,16 +6,17 @@ import {
   ResponseWithRecords,
 } from '../../../api/base-http-service/base-http.service';
 import { Customer } from '../../../other/models/Customer';
+import { MatIcon } from '@angular/material/icon';
 
 const DEFAULT_PAGINATION = { skip: 0, limit: 10 };
 const COLUMN_CONFIG = {
-  displayedColumns: ['name', 'createdAt', 'updatedAt'],
-  headers: ['Name', 'Erstellt am', 'Aktualisiert am'],
+  displayedColumns: ['name', 'createdAt', 'updatedAt', 'actions'],
+  headers: ['Name', 'Erstellt am', 'Aktualisiert am', ''],
 };
 
 @Component({
   selector: 'app-customer-table',
-  imports: [TemplateTableComponent],
+  imports: [TemplateTableComponent, MatIcon],
   templateUrl: './customer-table.component.html',
   styleUrl: './customer-table.component.scss',
 })
@@ -25,24 +26,36 @@ export class CustomerTableComponent implements OnInit {
   totalItems = signal(0);
   customerStore = inject(CustomersStore);
 
+  skip = signal(DEFAULT_PAGINATION.skip);
+  limit = signal(DEFAULT_PAGINATION.limit);
+
   async ngOnInit(): Promise<void> {
-    await this.loadCustomers({
-      skip: DEFAULT_PAGINATION.skip,
-      limit: DEFAULT_PAGINATION.limit,
-    });
+    await this.loadCustomers();
   }
 
-  async onPaginationChange(event: {
-    skip: number;
-    limit: number;
-  }): Promise<void> {
-    await this.loadCustomers(event);
+  async onPaginationChange(event: BaseQueryParams): Promise<void> {
+    this.skip.set(event.skip ?? 0);
+    this.limit.set(event.limit ?? 0);
+
+    await this.loadCustomers();
   }
 
-  private async loadCustomers(payload: BaseQueryParams): Promise<void> {
+  async deleteCustomer(id: string): Promise<void> {
+    try {
+      await this.customerStore.deleteCustomerByIdPromise(id);
+      await this.loadCustomers();
+    } catch (error) {
+      console.error(`Failed to delete customer with ID ${id}:`, error);
+    }
+  }
+
+  private async loadCustomers(): Promise<void> {
     try {
       const response: ResponseWithRecords<Customer> =
-        await this.customerStore.getAllCustomersPromise(payload);
+        await this.customerStore.getAllCustomersPromise({
+          skip: this.skip(),
+          limit: this.limit(),
+        });
 
       this.totalItems.set(response.total);
     } catch (error) {
--- src/app/shared/template-table/template-table.component.ts
+++ src/app/shared/template-table/template-table.component.ts
@@ -3,27 +3,42 @@ import {
   Component,
   DestroyRef,
   inject,
+  Input,
   input,
   output,
+  TemplateRef,
   ViewChild,
 } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { DatePipe, NgIf, NgTemplateOutlet } from '@angular/common';
+import { IsDatePipe } from '../../other/pipes/is-date.pipe';
 
 @Component({
   selector: 'app-template-table',
   standalone: true,
-  imports: [MatTableModule, MatPaginator],
+  imports: [
+    MatTableModule,
+    MatPaginator,
+    NgIf,
+    NgTemplateOutlet,
+    DatePipe,
+    IsDatePipe,
+  ],
   templateUrl: './template-table.component.html',
   styleUrl: './template-table.component.scss',
 })
 export class TemplateTableComponent<T> implements AfterViewInit {
   headers = input.required<string[]>();
   displayedColumns = input.required<string[]>();
+  @Input() cellTemplatesMap: { [key: string]: TemplateRef<any> } = {};
+
   tableData = input.required<T[]>();
-  pageSizes = input<number[]>([10, 25, 100]);
+
+  pageSizes = input<number[]>([5, 10, 25, 100]);
+  initialPageSize = input<number>(10);
   totalItems = input<number>();
 
   paginationChange = output<{ skip: number; limit: number }>();
@@ -67,5 +82,19 @@ export class TemplateTableComponent<T> implements AfterViewInit {
     if (this.sort) {
       this.dataSource.sort = this.sort;
     }
+  }
+
+  extractNestedProperty(item: any, key: string): any {
+    const keys = key.split('.');
+    let value = item;
+
+    for (const k of keys) {
+      if (value && Object.hasOwn(value, k)) {
+        value = value[k];
+      } else {
+        return null;
+      }
+    }
+    return value;
   }
 }
