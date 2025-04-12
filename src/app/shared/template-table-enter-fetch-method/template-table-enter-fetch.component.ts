759e856e80519e7c1b03eb4aae83ad384040d2e9
joscha-satt <j.sattler@28apps.de>
2025-04-12 19:14:47 +0200

    Add CommonModule to imports in template-table-fetch component
    CommonModule was added to the component to ensure access to Angular common directives and features. This change resolves potential issues with templates and enhances compatibility and functionality.

--- src/app/shared/template-table-enter-fetch-method/template-table-enter-fetch.component.ts
+++ src/app/shared/template-table-enter-fetch-method/template-table-enter-fetch.component.ts
@@ -29,7 +29,7 @@ import {
   tap,
 } from 'rxjs';
 import { ResponseWithRecords } from '../../api/base-http-service/base-http.service';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 export type FetchDataFunction<T> = (
   params: BaseGetQueryParams,
@@ -38,7 +38,7 @@ export type FetchDataFunction<T> = (
 @Component({
   selector: 'app-template-table-fetch',
   standalone: true,
-  imports: [MatTableModule, MatPaginator, DatePipe, IsDatePipe],
+  imports: [MatTableModule, MatPaginator, DatePipe, IsDatePipe, CommonModule],
   templateUrl: './template-table-enter-fetch.component.html',
   styleUrls: ['./template-table-enter-fetch.component.scss'],
 })
