30110a846925ce0fc8767b2563346c2c388d7eef
joscha-satt <j.sattler@28apps.de>
2026-05-17 02:03:02 +0200

    init autocomplete, wip features

--- src/app/shared/template-select-autocomplete/template-select-autocomplete.ts
+++ src/app/shared/template-select-autocomplete/template-select-autocomplete.ts
@@ -1,10 +1,14 @@
+import { NgTemplateOutlet } from '@angular/common';
 import {
   Component,
+  contentChild,
   input,
   type InputSignal,
   model,
   type ModelSignal,
   signal,
+  type Signal,
+  TemplateRef,
   type WritableSignal,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -17,7 +21,7 @@ export type SelectOptions = {
 
 @Component({
   selector: 'app-template-select-autocomplete',
-  imports: [FormsModule, AutoCompleteModule],
+  imports: [FormsModule, AutoCompleteModule, NgTemplateOutlet],
   templateUrl: './template-select-autocomplete.html',
   styleUrl: './template-select-autocomplete.scss',
 })
@@ -33,6 +37,11 @@ export class TemplateSelectAutocomplete {
 
   // signals
   readonly suggestedOptions: WritableSignal<Array<SelectOptions>> = signal([]);
+
+  // templates
+  readonly itemTemplate: Signal<TemplateRef<unknown> | undefined> = contentChild('item', { read: TemplateRef });
+  readonly headerTemplate: Signal<TemplateRef<unknown> | undefined> = contentChild('header', { read: TemplateRef });
+  readonly footerTemplate: Signal<TemplateRef<unknown> | undefined> = contentChild('footer', { read: TemplateRef });
 
   search(event: AutoCompleteCompleteEvent): void {
     const query: string = event.query.toLowerCase();
