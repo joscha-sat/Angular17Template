bb3d48e58c203b41770e9ff8dae4407aacd8b6a0
joscha-satt <j.sattler@28apps.de>
2026-07-09 19:41:56 +0200

    wip signal form example

--- src/app/shared/template-input/template-input.ts
+++ src/app/shared/template-input/template-input.ts
@@ -4,11 +4,12 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputText } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { ControlContainer, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
+import { FieldTree, FormField } from '@angular/forms/signals';
 import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-template-input',
-  imports: [IconFieldModule, InputIconModule, InputText, FloatLabel, ReactiveFormsModule, TranslocoPipe],
+  imports: [IconFieldModule, InputIconModule, InputText, FloatLabel, ReactiveFormsModule, FormField, TranslocoPipe],
   templateUrl: './template-input.html',
   styleUrl: './template-input.scss',
   viewProviders: [
@@ -20,7 +21,8 @@ import { TranslocoPipe } from '@jsverse/transloco';
 })
 export class TemplateInput {
   readonly label: InputSignal<string> = input('');
-  readonly fControlName: InputSignal<string> = input.required<string>();
+  readonly fControlName: InputSignal<string> = input('');
+  readonly field: InputSignal<FieldTree<string> | undefined> = input<FieldTree<string> | undefined>(undefined);
   readonly type: InputSignal<'text' | 'password'> = input<'text' | 'password'>('text');
   readonly iconClass: InputSignal<string> = input('');
 }
--- src/app/views/test-view/test-view.ts
+++ src/app/views/test-view/test-view.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { Component, signal, type WritableSignal } from '@angular/core';
+import { email, type FieldTree, form, required, submit } from '@angular/forms/signals';
+import { TemplateInput } from '../../shared/template-input/template-input';
 import { TemplateSelectAutocomplete } from '../../shared/template-select-autocomplete/template-select-autocomplete';
+
+type UserFormTypes = {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+};
 
 @Component({
   selector: 'app-test-view',
-  imports: [ReactiveFormsModule, TemplateSelectAutocomplete],
+  imports: [TemplateSelectAutocomplete, TemplateInput, JsonPipe],
   templateUrl: './test-view.html',
   styleUrl: './test-view.scss',
 })
-export class TestView {}
+export class TestView {
+  protected readonly userFormObject: WritableSignal<UserFormTypes> = signal<UserFormTypes>({
+    firstName: '',
+    lastName: '',
+    emailAddress: '',
+  });
+
+  protected readonly userForm: FieldTree<UserFormTypes> = form(this.userFormObject, (formSchemaPath) => {
+    // firstName
+    required(formSchemaPath.firstName, { message: 'Vorname ist ein Pflichtfeld!' });
+    // lastName
+    required(formSchemaPath.lastName);
+    // email
+    required(formSchemaPath.emailAddress);
+    email(formSchemaPath.emailAddress);
+  });
+
+  protected readonly submittedSignalFormValue: WritableSignal<UserFormTypes | null> = signal<UserFormTypes | null>(
+    null,
+  );
+
+  protected submitSignalFormExample(): void {
+    submit(this.userForm, async () => {
+      this.submittedSignalFormValue.set(this.userFormObject());
+      return undefined;
+    });
+  }
+}
