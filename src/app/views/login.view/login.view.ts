0655822ec3609287f023d62565a2796e00691f0f
joscha-satt <j.sattler@28apps.de>
2026-07-21 19:33:21 +0200

    wip update primeng

--- src/app/views/login.view/login.view.ts
+++ src/app/views/login.view/login.view.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginForm } from '../../components/login/login-form/login-form';
-import { CardModule } from 'primeng/card';
 import { LoginHeader } from '../../components/login/login-header/login-header';
 
 @Component({
-  selector: 'app-login.view',
-  imports: [LoginForm, CardModule, LoginHeader],
+  selector: 'app-login',
+  imports: [LoginForm, LoginHeader],
   templateUrl: './login.view.html',
   styleUrl: './login.view.scss',
 })
